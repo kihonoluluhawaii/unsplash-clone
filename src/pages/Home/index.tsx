dfb4c518926ab111d3aaf00dbe03cdf5969d4809
kiho <kihonoluluhawaii@gmail.com>
2024-10-24 14:42:18 +0900

    feat: usePhoto, Photos 컴포넌트 완성

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,16 +1,19 @@
 import styled from "@emotion/styled";
 import MainHeader from "@/pages/Home/components/MainHeader.tsx";
+import Photos from "@/components/photos";
 
 const Home = () => {
   return (
     <Container>
       <MainHeader />
+      <Photos />
     </Container>
   );
 };
 
 const Container = styled.div`
   display: flex;
+  flex-direction: column;
   align-items: center;
   margin: 0 auto;
   max-width: 1300px;
